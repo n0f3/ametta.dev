6e0dccba1ebfc902a3ddc0e8c55af3c2a74bf9c1
n0f3 <n0f3@outlook.com>
2020-09-21 23:52:19 -0400

    Added basic theme configuration with tailwind css

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,8 +3,8 @@ import styles from './layout.module.css';
 import utilStyles from 'styles/utils.module.css';
 import Link from 'next/link';
 
-const name = '[Your Name]';
-export const siteTitle = 'Next.js Sample Website';
+const name = '';
+export const siteTitle = 'Alessandro Metta Portfolio';
 
 export default function Layout({
   children,
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'accent-1': '#333',
+        'dracula-background': '#22212C',
+        'dracula-comment': '#7970A9',
+        'dracula-foreground': '#F8F8F2',
+        'dracula-selection': '#454158',
+        'dracula-green': '#8AFF80',
+        'dracula-pink': '#FF80BF',
+        'dracula-purple': '#FF80BF',
+        'dracula-cyan': '#80FFEA',
+        'dracula-orange': '#FFCA80',
+        'dracula-red': '#FF9580',
+        'dracula-yellow': '#FFFF80',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
